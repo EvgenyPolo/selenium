ceb57a743963c391e8d532d7c7c3b9bb854ce5a6
EvgenyPolo <3670242@gmail.com>
2022-05-11 17:01:28 +0700

    Initial commit

---
+++ settings.py
@@ -0,0 +1,30 @@
+url = "https://petfriends.herokuapp.com/"
+
+valid_email = "reg_adr3_nsk@mail.ru"
+valid_password = "pet12345"
+
+reg_name = "reg_adr_4_nsk"
+reg_email = "reg_adr_4_nsk@mail.ru"
+reg_password = "pet12345"
+reg_screenshot_file = 'result_reg_petfriends.png'
+reg_err_screenshot_file = 'error_reg_petfriends.png'
+
+allpet_screenshot_file = 'all_petfriends.png'
+mypet_screenshot_file = 'my_petfriends.png'
+
+css_my_pets_images = 'div#all_my_pets table tbody tr th img'
+css_my_pets_names = 'tbody tr td:nth-of-type(1)'
+css_my_pets_types = 'tbody tr td:nth-of-type(2)'
+css_my_pets_ages = 'tbody tr td:nth-of-type(3)'
+css_my_pets_stat = 'div.\.col-sm-4.left'
+css_my_pets_submit = 'button[type="submit"]'
+
+link_text_btn_exist_acc = u"У меня уже есть аккаунт"
+xpath_navbar_my_pets = "//*[@id = 'navbarNav']/ul[1]/li[1]/a[1]"
+xpath_all_pets_submit = "//button[@type='submit']"
+xpath_btn_newuser = "//button[@onclick=\"document.location='/new_user';\"]"
+xpath_btn_reg_newuser = "//body/div[1]/div[1]/form[1]/div[4]/button[1]"
+
+path_by_driver = r"C:\Users\Евгений\PycharmProjects\pythonProject\selenium"
+driver_firefox = r"C:\Users\Евгений\PycharmProjects\pythonProject\selenium\geckodriver.exe"
+driver_chrome = r"C:\Users\Евгений\PycharmProjects\pythonProject\selenium\chromedriver.exe"
